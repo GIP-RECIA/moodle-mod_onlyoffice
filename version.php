a8beb848741d4e9b472c032c0ab590326c823121
lvillanne-recia <loic.villanne@recia.fr>
2023-02-23 16:05:54 +0100

    Création d'une nouvelle version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_onlyoffice';
-$plugin->version = 2022120300;
+$plugin->version = 2022120301;
 $plugin->release = 'v4.1_7.1'; // <Moodle version>_<OnlyOffice version>
 $plugin->requires = 2017111301;
 $plugin->maturity = MATURITY_STABLE;
